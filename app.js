a8520f598d782d5aba94a498e8b3d3477d18b3f1
KLesiu <kubaxl222@gmail.com>
2022-11-14 22:12:59 +0100

    Add three ford"s photos. Add slide gallery at section fiesta. Use propety setAtribiute to show next photo at the slider.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mondeo = document.querySelector(".mondeo__menu");
 const empty = document.querySelector(".emptyPlace1");
 const emptyTwo = document.querySelector(".emptyPlace2");
 const mustang = document.querySelector(".mustang__menu");
+const arrowRight = document.querySelector(".arrow__right");
 let test;
 
 let switchSlide = [gokart, fiesta, mondeo, mustang];
@@ -24,13 +25,15 @@ const change = () => {
   }
 };
 const showVideoFiesta = () => {
-  const imageFiesta = document.querySelector(".fiesta__image");
+  const imageFiesta = document.querySelector("#fiestaPhotoId");
   imageFiesta.classList.toggle("hidden");
   let buttonText = document.querySelector(".fiesta__button");
   if (imageFiesta.classList.contains("hidden")) {
     buttonText.innerText = "Look at the photo";
+    arrowRight.classList.add("hidden");
   } else {
     buttonText.innerText = "Look at the video";
+    arrowRight.classList.remove("hidden");
   }
 
   const videoFiesta = document.querySelector(".fiesta__video");
@@ -106,6 +109,23 @@ const show = () => {
   switchSlide[2].addEventListener("click", changeTwo);
   switchSlide[3].addEventListener("click", changeThree);
 };
+const sliderFiesta = () => {
+  let imageFiesta = document.querySelector("#fiestaPhotoId");
+  if (imageFiesta.classList.contains("fiesta__image1")) {
+    imageFiesta.setAttribute("src", "img/fiesta2.jpg");
+    imageFiesta.setAttribute("class", "fiesta__image2");
+  } else if (imageFiesta.classList.contains("fiesta__image2")) {
+    imageFiesta.setAttribute("src", "img/fiesta3.jpg");
+    imageFiesta.setAttribute("class", "fiesta__image3");
+  } else {
+    imageFiesta.setAttribute("src", "img/fiesta1.jpg");
+    imageFiesta.setAttribute("class", "fiesta__image1");
+  }
+};
+const buttonOfSliderFiesta = () => {
+  arrowRight.addEventListener("click", sliderFiesta);
+};
 
 show();
 changeText();
+buttonOfSliderFiesta();
